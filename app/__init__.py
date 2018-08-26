494b3463e6d2a41ca803056584ba453bfb9c7451
baijiewen <15364719037@163.com>
2018-08-26 21:20:40 +0800

    add celery

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
+from flask_mail import Mail
 from config import config
+from celery import Celery
 
 
 db = SQLAlchemy()
+celery = Celery()
+mail = Mail()
 
 
 def create_app(config_name):
@@ -14,11 +18,21 @@ def create_app(config_name):
     api1 = Api()
     db.init_app(app)
 
+    celery.__init__(app.name, broker=app.config['CELERY_BROKER_URL'])
+    # # celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+    # print app.name
+    celery.conf.update(app.config)
+    # print celery._preconf['BROKER_URL']
+    mail.init_app(app)
+
     from .api_1_0.goods import GoodsAPI
     api1.add_resource(GoodsAPI, '/api/v2.0/goods', endpoint='goods')
     api1.init_app(app)
 
     from .api_1_0 import api as api_1_0_blueprint
     app.register_blueprint(api_1_0_blueprint, url_prefix='/api/v1.0')
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     return app
--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 import os
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
-
+from celery import Celery
 from app import create_app, db
 from app.models import User
 
@@ -10,9 +10,14 @@ app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
 
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+
+
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db,User=User)
+
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command("db", MigrateCommand)
